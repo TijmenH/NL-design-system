0ef3c39da2a260acc575a4ac925e03fd4fbce043
Tijmen Helder <tijmen.helder@rebels.io>
2026-06-19 15:35:25 +0200

    Add title to app header

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -1,5 +1,10 @@
-import { PageHeader } from "@utrecht/component-library-react";
+import { Heading1, PageHeader } from "@utrecht/component-library-react";
+import styles from "./AppHeader.module.css";
 
 export function AppHeader() {
-  return <PageHeader />;
+  return (
+    <PageHeader className={styles.header}>
+      <Heading1 className={styles.title}>NL Design System</Heading1>
+    </PageHeader>
+  );
 }
